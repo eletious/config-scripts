94d465eeec0dfe739cb0ef39546795a404ccee4b
Geoff Sheldon <geoffsheldon@live.com>
2018-05-19 15:31:31 -0700

    added automatic wallpaper getting/setting

--- get-configs.sh
+++ get-configs.sh
@@ -2,6 +2,9 @@
 
 ## collect scripts
 
+# collect wallpaper
+cp ~/Pictures/wallpaper.jpg ~/config-scripts/images/wallpaper.jpg
+
 # tmux
 cp ~/.tmux.conf ~/config-scripts/dotfiles/.tmux.conf
 cp ~/.config/i3/config ~/config-scripts/dotfiles/i3-config
--- set-configs.sh
+++ set-configs.sh
@@ -1,5 +1,8 @@
 #! /bin/bash
 
+# copies wallpaper from repo to usable location
+cp ~/config-scripts/images/wallpaper.jpg ~/Pictures/wallpaper.jpg
+
 # copy configs from repo to folder
 cp ~/config-scripts/dotfiles/.tmux.conf ~/.tmux.conf
 cp ~/config-scripts/dotfiles/i3-config ~/.config/i3/config
