e0b20c378808eb8f2eab7cc9e7292614e5b2f99e
Geoff Sheldon <geoffsheldon@live.com>
2018-05-19 13:56:23 -0700

    added setter script, and steps to install i3 gaps

---
+++ set-configs.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+
+# copy configs from repo to folder
+cp ~/config-scripts/dotiles/.tmux.conf ~/.tmux.conf
+cp ~/config-scripts/dotfiles/.i3-config ~/.config/i3/config
--- setup.sh
+++ setup.sh
@@ -1,11 +1,50 @@
 #! /bin/bash
 
+# setup folders
+
+cd
+mkdir development
+mkdir sources
+
 # install packages
 apt install -y i3 \
 	tmux \
 	htop \
-	git
+	git \
+	libxcb1-dev \
+	libxcb-keysyms1-dev \
+	libpango1.0-dev \
+	libxcb-util0-dev \
+	libxcb-icccm4-dev \
+	libyajl-dev \
+	libstartup-notification0-dev \
+	libxcb-randr0-dev \
+	libev-dev \
+	libxcb-cursor-dev \
+	libxcb-xinerama0-dev \
+	libxcb-xkb-dev \
+	libxkbcommon-dev \
+	libxkbcommon-x11-dev autoconf \
+	libxcb-xrm0 \
+	libxcb-xrm-dev \
+	automake
+
+###### install i3-gaps
+
+cd ~/sources
+git clone https://github.com/Airblader/i3 i3-gaps
+cd i3-gaps
+autoreconf --force --install
+mkdir -p build && cd build/
+../configure --prefix=/usr --sysconfdir=/etc --disable sanitizers
+make
+sudo make install
+#####
 
 # copy configs
 cp ~/config-scripts/dotiles/.tmux.conf ~/.tmux.conf
 cp ~/config-scripts/dotfiles/.i3-config ~/.config/i3/config
+
+
+### LAST STEP!  Reboot computer ###
+sudo reboot now
